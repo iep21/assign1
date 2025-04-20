874e6e714d5fee5d8b62e06cdf0a58280ca3ff11
iep21 <iep21@fsu.edu>
2025-04-20 18:05:15 -0400

    Create product.cs

---
+++ product.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring2025_Samples.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name} - ${Price} - {Stock} in stock";
+            }
+        }
+
+        public Product()
+        {
+            Name = string.Empty;
+            Price = 0.0m; //default price
+            Stock = 0; //default stock
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+    }
+    public class CartItem
+    {
+        public Product Product { get; set; }//stores product object
+        public int Quantity { get; set; }//stores quantity of the product in cart
+
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Product.Name} x{Quantity} - ${Product.Price:F2} each";
+        }
+
+        public decimal TotalPrice()
+        {
+            return Product.Price * Quantity;
+        }
+    }
+
+
+}
